5abba8ea72a4cf5e33fd3192090b4dc89abe547f
Nlbeauty <nlbeauty31@gmail.com>
2026-04-08 22:53:27 +0200

    Create Vite.config.js

---
+++ Vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'NL Beauty',
+        short_name: 'NL Beauty',
+        description: 'Réservez vos soins en ligne',
+        theme_color: '#faf7f4',
+        background_color: '#faf7f4',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
